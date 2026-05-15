8dc9b11c55368ae09935e17a8aa4535255ab7442
Momsnoodle <wenyi.jiang0508@gmail.com>
2026-05-15 16:01:07 +0200

    Main editing

--- Main.sv
+++ Main.sv
@@ -1,3 +1,22 @@
+module Main#(
+
+
+
+
+)(
+  input        MAX10_CLK1_50,  // 50 HMz clock
+  input  [1:0] KEY,            // Buttons
+  inout  [9:0] ARDUINO_IO,     // Header pins
+  output [9:0] LEDR,           // LEDs
+  input  [9:0] SW,             // Switches
+  output [7:0] HEX0,           // 7-segment display
+  output [7:0] HEX1,           // 7-segment display
+  output [7:0] HEX2,           // 7-segment display
+  output [7:0] HEX3,           // 7-segment display
+  output [7:0] HEX4            // 7-segment display
+);
+
+
 logic [99:0] generated_sequence;
 
 LFSR_Sequence_Generator seq_gen (
